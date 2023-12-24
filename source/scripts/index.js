314c2a1b7affad65ba5175e53331417e739b9067
Александра Блинова <blinovaao@yandex.ru>
2023-12-25 00:34:42 +0300

    Добавляет слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+
+const compareSliderToggle = document.querySelector(".compare-slider__toggle");
+const compareSlider = document.querySelector(".compare-slider");
+
+let flag = false;
+compareSliderToggle.addEventListener('click', ()=> {
+  if (flag) {
+    compareSlider.classList.toggle("compare-slider--after");
+    compareSlider.classList.toggle("compare-slider--before");
+  } else {
+    flag = true;
+
+    compareSlider.classList.add("compare-slider--after");
+  }
+})
