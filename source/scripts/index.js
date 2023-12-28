ac025bc8043908c470919f74b33de85cd6c356da
Александра Блинова <blinovaao@yandex.ru>
2023-12-28 09:36:41 +0300

    Исправляет ошибки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,27 +1,27 @@
-/* в этот файл добавляет скрипты*/
-
-
-/* SLIDER */
 const compareSliderToggle = document.querySelector(".compare-slider__toggle");
 const compareSlider = document.querySelector(".compare-slider");
 
-let flag = false;
-compareSliderToggle.addEventListener('click', ()=> {
-  if (flag) {
-    compareSlider.classList.toggle("compare-slider--after");
-    compareSlider.classList.toggle("compare-slider--before");
-  } else {
-    flag = true;
-
-    compareSlider.classList.add("compare-slider--after");
-  }
-})
-
-
-/* HEADER-MENU */
 const mainNavToggle = document.querySelector(".main-nav__toggle");
 const mainNavList = document.querySelector(".main-nav__list");
 
+
+/* SLIDER */
+if (compareSliderToggle) {
+  let flag = false;
+  compareSliderToggle.addEventListener('click', ()=> {
+    if (flag) {
+      compareSlider.classList.toggle("compare-slider--after");
+      compareSlider.classList.toggle("compare-slider--before");
+    } else {
+      flag = true;
+
+      compareSlider.classList.add("compare-slider--after");
+    }
+  })
+}
+
+
+/* HEADER-MENU */
 mainNavToggle.addEventListener('click', ()=> {
   mainNavList.classList.toggle("main-nav__list--opened");
   mainNavToggle.classList.toggle("main-nav__toggle--opened");
